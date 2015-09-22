81f5493ba9b70684d9c0c4dfaff558522c3c7048
Rhodri James <rhodrialunjames@hotmail.com>
2015-09-22 13:42:02 +0100

    Fresh install of Opencart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/milcart/admin/');
+define('HTTP_CATALOG', 'http://localhost/milcart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/milcart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/milcart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/milcart/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/milcart/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/milcart/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/milcart/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/milcart/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/milcart/image/');
+define('DIR_CACHE', 'C:/wamp/www/milcart/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/milcart/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/milcart/system/upload/');
+define('DIR_LOGS', 'C:/wamp/www/milcart/system/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/milcart/system/modification/');
+define('DIR_CATALOG', 'C:/wamp/www/milcart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'milcart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/milcart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/milcart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/milcart/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/milcart/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/milcart/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/milcart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/milcart/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/milcart/image/');
+define('DIR_CACHE', 'C:/wamp/www/milcart/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/milcart/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/milcart/system/upload/');
+define('DIR_MODIFICATION', 'C:/wamp/www/milcart/system/modification/');
+define('DIR_LOGS', 'C:/wamp/www/milcart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'milcart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
